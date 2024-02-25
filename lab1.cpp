3d3bebb7b875f1e43093a4e29bde12dfb3bbd366
sparky <buchinskii.sv@phystech.edu>
2024-02-25 16:37:42 +0300

    commit message 4

--- lab1.cpp
+++ lab1.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <chrono>
+#include <random>
+
+
+// program will track the time of executing this func
 
 void func()
-{
-    std::cout << "Hello world" << '\r';
+{ 
+  std::cout << "Za praVdu" << '\r';
 }
 
-int main() 
+int main()
 {
-    auto begin = std::chrono::steady_clock::now(); 
-    for (unsigned cnt = 100000; cnt != 0; --cnt)
-        func();
-    auto end = std::chrono::steady_clock::now();  
-    auto time_span = std::chrono::duration_cast < std::chrono::milliseconds > (end - begin);
-    
-    std::cout << "\n\n";
-    std::cout << time_span.count() << std::endl;
+  auto begin = std::chrono::steady_clock::now();
+  for (unsigned cnt = 100000; cnt != 0; --cnt)
+    func();
+  auto end = std::chrono::steady_clock::now();
+  auto time_span = std::chrono::duration_cast < std::chrono::milliseconds > (end - begin);  
 
-    return 0;
+  std::cout << "\n\n";
+  std::cout << time_span.count() << std::endl;
+  return 0;
 }
+
