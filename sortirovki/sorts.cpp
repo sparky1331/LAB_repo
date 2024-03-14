5b4887b011e6689fa355cea990f36acf8103e5d9
sparky <buchinskii.sv@phystech.edu>
2024-03-14 18:27:27 +0300

    sorts commit

--- sortirovki/sorts.cpp
+++ sortirovki/sorts.cpp
@@ -2,9 +2,45 @@
 #include <random>
 
 
+// merges sorted arr1[n1] arr2[n2] in sorted arr[n1 + n2]
+void merge(int n1, int n2, int* arr1, int* arr2, int* arr) {
+
+    int tmp1 = 0, tmp2 = 0;
+    
+    for (int i = 0; i < n1 + n2; i++) {
+    
+        if ((tmp1 < n1) and (tmp2 < n2)) {
+          
+          if (arr1[tmp1] <= arr2[tmp2]) {  
+           
+            arr[i] = arr1[tmp1]; 
+            tmp1++;
+          
+          }        
+        
+          else {
+
+          arr[i] = arr2[tmp2];
+          tmp2++;
+        
+          }
+        
+        }
+    
+        else {
+
+          arr[i] = arr1[tmp1] * (tmp1 < n1) + arr2[tmp2] * (tmp2 < n2);
+          tmp1 = tmp1 + (tmp1 < (n1 - 1));
+          tmp2 = tmp2 + (tmp2 < (n2 - 1));
+
+        }
+    
+    }
+
+}
 
 // swaps arr[i] and arr[j] in given array
-void swap(int i, int j, int arr[]) {
+void swap(int i, int j, int* arr) {
 
     int tmp = arr[i];
     arr[i] = arr[j];
@@ -13,7 +49,7 @@ void swap(int i, int j, int arr[]) {
 }
 
 // k - largest number in generated array, n - number of elements in array, arr - generated array
-void rand(int k, int n, int arr[]) {  
+void rand(int k, int n, int* arr) {  
 
   unsigned seed = 1001; 
   std::default_random_engine rng(seed);
@@ -28,7 +64,53 @@ void rand(int k, int n, int arr[]) {
 
 }  
 
-void puzyrek(int n, int arr[]) {
+void heapify(int i, int n, int* arr) {
+
+    int left = 2*i;
+    int right = 2*i + 1;
+    int largest = i;
+    
+    if ((left <= n) and (arr[left] > arr[largest]))
+
+        largest = left;
+
+    if ((right <= n) and (arr[right] > arr[largest]))
+
+        largest = right;
+    
+    if (largest != i) {
+
+        swap(i, largest, arr);
+        heapify(largest, n, arr);
+
+    }
+
+}
+
+
+void build_heap(int n, int* arr) {
+
+    for (int i = n / 2; i > -1; --i) 
+
+        heapify(i, n, arr);
+
+}
+
+void heapsort(int n, int* arr) {
+
+    build_heap(n, arr);
+
+    for (int i = n; i != -1; --i) {
+
+        swap(i, 0, arr);
+        --n;
+        heapify(0, n, arr);
+
+    }
+
+}
+
+void puzyrek(int n, int *arr) {
 
     for (int i = n - 1; i != 0; --i) {
 
@@ -43,7 +125,7 @@ void puzyrek(int n, int arr[]) {
     }
 }
 
-void vybor(int n, int arr[]) {
+void vybor(int n, int* arr) {
 
     for (int i = 0; i < n - 1; i++) {
 
@@ -65,7 +147,7 @@ void vybor(int n, int arr[]) {
 
 }
 
-void vstavka(int n, int arr[]) {
+void vstavka(int n, int* arr) {
 
     for (int i = 0; i < n - 1; i++) {
 
@@ -84,7 +166,7 @@ void vstavka(int n, int arr[]) {
 }
 
 // sorts the array from n_1'th to n_2'th (arr[n_1:n_2]) elements. If you want to sort whole array: n_1 = 0, n_2 = n, where n is the length of the array.
-void qsort(int n_1, int n_2 , int arr[]) {
+void qsort(int n_1, int n_2 , int * arr) {
 
     if (n_2 - n_1 > 2) {
 
@@ -149,6 +231,32 @@ void qsort(int n_1, int n_2 , int arr[]) {
 
 }
 
+void mergesort(int n, int * arr) {
+    
+    if (n > 2) {
+        int * temp_arr = new int[n];
+        mergesort(n/2, arr);
+        mergesort(n - n/2, arr + n - n/2);
+        merge(n/2, n - n/2, arr, arr + n - n/2, temp_arr);
+        
+        for (int i = 0; i < n; i++) 
+
+            arr[i] = temp_arr[i];
+
+        delete[] temp_arr;
+
+    }
+    
+    else {
+
+        if (arr[0] > arr[1])
+
+            swap(0, 1, arr);
+
+    } 
+
+}
+
 
 int main() {
  
@@ -156,7 +264,7 @@ int main() {
     std::cout << "enter number of items in arr, then largest number" << std::endl;
     std::cin >> n >> k;
     
-    int arr[n];
+    int* arr = new int[n];
 
     rand(k, n, arr);
 
@@ -169,7 +277,7 @@ int main() {
     std::cout << "" << std::endl;
 
     // choose sort alg:
-    qsort(0, n, arr);
+    mergesort(n, arr);
 
     for (int i = 0; i < n; i++) {
 
